46b9cd125c70ff6a68b26710a6da4479c64cd566
oc34n <ocean.wrng@gmail.com>
2023-07-12 22:22:45 -0400

    Added basic styling and elements for site layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.scss'
+import React from 'react';
+import './App.scss';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <main>
+      <section className='ticketDisplay'>
+        <button>Generate Ticket</button>
+      </section>
+      <section className='formSection'>
+        <form>
+          <label htmlFor='Ticketholder Name'>
+            Ticketholder Name
+            <input/>
+          </label>
+          <label htmlFor='Ticket ID Number'>
+            Ticket ID Number
+            <input/>
+          </label>
+          <div className='dateID'>
+          <label className='date' htmlFor='Ticket Date (MM/DD/YY)'>
+            Date (MM/DD/YY)
+            <div className='dateInfo'>
+              <input/>
+              <input/>
+              <input/>
+            </div>
+          </label>
+          <label htmlFor='Ticket Unique ID'>
+            Unique ID
+            <input/>
+          </label>
+          </div>
+          <button>Verify Ticket!</button>
+        </form>
+      </section>
+    </main>
   )
 }
 
